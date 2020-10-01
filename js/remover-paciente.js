8ac5f29d6be903c095d505a7e85b26440af1a3f8
Thalles Bruno <thallesbgns@gmail.com>
2020-10-01 11:22:45 -0300

    event.target parentNode

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -1,8 +1,22 @@
 var pacientes = document.querySelectorAll(".paciente");
 
-pacientes.forEach(function(paciente){
-    paciente.addEventListener("dblclick", function (){
-        //console.log("Fui clicado com duplo clique.");
-        this.remove();
-    });
+var tabela = document.querySelector("table");
+
+tabela.addEventListener("dblclick", function (event){
+    // console.log(event.target);
+    // console.log(this);
+
+    var alvoEvento = event.target;
+    var paiDoAlvo = alvoEvento.parentNode; //tr = paciente = remover
+
+    paiDoAlvo.remove();
+
+
 });
+
+// pacientes.forEach(function(paciente){
+//     paciente.addEventListener("dblclick", function (){
+//         //console.log("Fui clicado com duplo clique.");
+//         this.remove();
+//     });
+// });
